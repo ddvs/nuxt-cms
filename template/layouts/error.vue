92078a8c2d736087fbb20f1aded9d85138ab7f90
hadiChan <861883474@qq.com>
2017-01-03 17:08:27 +0800

    edit
    something …

--- template/layouts/error.vue
+++ template/layouts/error.vue
@@ -11,26 +11,36 @@
 		</nuxt-link>
 	</section>
 </template>
+
 <script>
 export default {
-	props: ['error']
+	props: ['error'],
+	head(){
+		return {
+			title:`屏趣 -- ${this.error.statusCode}`,
+		    meta:[
+		        {'charset': 'utf-8' },
+				{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+		        {'name':'renderer','content':'webkit'}
+		    ]
+		}
+	}
 }
 </script>
 
 <style scoped>
-.title
-{
+.title{
 	margin-top: 15px;
 	font-size: 5em;
+	text-align: center;
 }
-.info
-{
+.info{
 	font-weight: 300;
 	color: #9aabb1;
 	margin: 0;
+	text-align: center;
 }
-.button
-{
+.button{
 	margin-top: 50px;
 }
 </style>
