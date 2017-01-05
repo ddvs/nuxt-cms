92a4f257f1c14efbbc782dad33dd04c40df098df
hadiChan <861883474@qq.com>
2017-01-05 20:40:15 +0800

    edit
    修改错误页面

--- template/layouts/error.vue
+++ template/layouts/error.vue
@@ -6,9 +6,9 @@
 		<h2 class="info">
 			{{ error.message }}
 		</h2>
-		<nuxt-link class="button" to="/" v-if="error.statusCode === 404">
+		<!-- <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
 			Homepage
-		</nuxt-link>
+		</nuxt-link> -->
 	</section>
 </template>
 
@@ -30,9 +30,15 @@ export default {
 
 <style scoped>
 .title{
-	margin-top: 15px;
-	font-size: 5em;
 	text-align: center;
+	color: #fff;
+    font-size: 150px;
+    line-height: 1;
+    margin-bottom: 20px;
+    font-weight: 300;
+    text-stroke: 1px transparent;
+    display: block;
+    text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,0.1), 0 0 5px rgba(0,0,0,0.1), 0 1px 3px rgba(0,0,0,0.3), 0 3px 5px rgba(0,0,0,0.2), 0 5px 10px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.2), 0 20px 20px rgba(0,0,0,0.15);
 }
 .info{
 	font-weight: 300;
