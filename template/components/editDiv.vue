81c0abc0a0414a38d912ee884409f34ddd84db92
hadiChan <861883474@qq.com>
2017-03-01 20:18:07 +0800

    add

---
+++ template/components/editDiv.vue
@@ -0,0 +1,22 @@
+<template>
+  <div contenteditable="true"
+         v-html="innerText"
+         @input="changeText"></div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data(){
+    return {
+      innerText:this.value
+    }
+  },
+  methods:{
+    changeText(){
+      this.innerText = this.$el.innerHTML;
+      this.$emit('input',this.innerText);
+    }
+  }
+}
+</script>
