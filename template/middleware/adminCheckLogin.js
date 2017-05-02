29230f5efaf313ee9b40d7de2d91cfacb95ef135
hadiChan <861883474@qq.com>
2017-05-02 14:54:27 +0800

    edit

--- template/middleware/adminCheckLogin.js
+++ template/middleware/adminCheckLogin.js
@@ -1,4 +1,4 @@
-export default function ({ store, redirect, route}) {
+export default function ({store, redirect, route}) {
   if (store.state.admin.isLogin === false && route.path !== '/admin/login') {
     return redirect('/admin/login')
   }
