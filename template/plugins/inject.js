5e8def62d6bc7a8f41616d57ec6af5004255c8da
AllenZho <1714080139@qq.com>
2018-05-24 15:20:39 +0800

    fixbug for inject.js

--- template/plugins/inject.js
+++ template/plugins/inject.js
@@ -26,68 +26,62 @@ function removeClass (elements, cName) {
   }
 }
 let directive = { // 指令
-  center: { // 图片居中切割
-    inserted: function (el, binding) {
-      el.onload = (e) => {
-        let w = el.naturalWidth
-        let h = el.naturalHeight
-        let pW = el.parentNode.offsetWidth
-        let pH = el.parentNode.offsetHeight
-        let ratio = w / h
-        let pRatio = pW / pH
-        if (pRatio > ratio) {
-          removeClass(el, 'Hcenter')
-          addClass(el, 'Vcenter')
-          // el.classList.remove('Hcenter')
-          // el.classList.add('Vcenter')
-        } else {
-          removeClass(el, 'Vcenter')
-          addClass(el, 'Hcenter')
-          // el.classList.remove('Vcenter')
-          // el.classList.add('Hcenter')
-        }
+  center: function (el, binding) { // 图片居中切割
+    el.onload = (e) => {
+      let w = el.naturalWidth
+      let h = el.naturalHeight
+      let pW = el.parentNode.offsetWidth
+      let pH = el.parentNode.offsetHeight
+      let ratio = w / h
+      let pRatio = pW / pH
+      if (pRatio > ratio) {
+        removeClass(el, 'Hcenter')
+        addClass(el, 'Vcenter')
+        // el.classList.remove('Hcenter')
+        // el.classList.add('Vcenter')
+      } else {
+        removeClass(el, 'Vcenter')
+        addClass(el, 'Hcenter')
+        // el.classList.remove('Vcenter')
+        // el.classList.add('Hcenter')
       }
     }
   },
-  flex: { // 容器自适应
-    inserted: function (el, binding) {
-      // el.classList.add('flex-box')
-      addClass(el, 'flex-box')
-      var div = document.createElement('div')
-      div.className = 'expansion'
-      div.style.paddingBottom = binding.value * 100 + '%'
-      el.appendChild(div)
-    }
+  flex: function (el, binding) { // 容器自适应
+    // el.classList.add('flex-box')
+    addClass(el, 'flex-box')
+    var div = document.createElement('div')
+    div.className = 'expansion'
+    div.style.paddingBottom = binding.value * 100 + '%'
+    el.appendChild(div)
   },
-  src: {
-    inserted: function (el, binding) {
-      setTimeout(() => {
-        let pW = el.parentNode.offsetWidth
-        let pH = el.parentNode.offsetHeight
-        if (binding.modifiers.center) {
-          el.src = binding.value + '?x-oss-process=image/resize,m_fill,h_' + parseInt(pH) + ',w_' + parseInt(pW)
-          el.onload = function (e) {
-            let w = el.naturalWidth
-            let h = el.naturalHeight
-            let ratio = w / h
-            let pRatio = pW / pH
-            if (pRatio > ratio) {
-              removeClass(el, 'Hcenter')
-              addClass(el, 'Vcenter')
-              // el.classList.remove('Hcenter')
-              // el.classList.add('Vcenter')
-            } else {
-              removeClass(el, 'Vcenter')
-              addClass(el, 'Hcenter')
-              // el.classList.remove('Vcenter')
-              // el.classList.add('Hcenter')
-            }
+  src: function (el, binding) {
+    setTimeout(() => {
+      let pW = el.parentNode.offsetWidth
+      let pH = el.parentNode.offsetHeight
+      if (binding.modifiers.center) {
+        el.src = binding.value + '?x-oss-process=image/resize,m_fill,h_' + parseInt(pH) + ',w_' + parseInt(pW)
+        el.onload = function (e) {
+          let w = el.naturalWidth
+          let h = el.naturalHeight
+          let ratio = w / h
+          let pRatio = pW / pH
+          if (pRatio > ratio) {
+            removeClass(el, 'Hcenter')
+            addClass(el, 'Vcenter')
+            // el.classList.remove('Hcenter')
+            // el.classList.add('Vcenter')
+          } else {
+            removeClass(el, 'Vcenter')
+            addClass(el, 'Hcenter')
+            // el.classList.remove('Vcenter')
+            // el.classList.add('Hcenter')
           }
-        } else {
-          el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
         }
-      }, 10)
-    }
+      } else {
+        el.src = binding.value + '?x-oss-process=image/resize,m_lfit,h_' + parseInt(pH) + ',w_' + parseInt(pW)
+      }
+    }, 10)
   }
 }
 
