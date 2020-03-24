8efe42ed51b9bd98acbce4c535c0761e55cfb741
HadiChan <15089581429@163.com>
2020-03-24 18:10:01 +0800

    eslintrc.js

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -25,14 +25,35 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // do not allow console.logs etc...
-    'no-console': 2
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "generator-star-spacing": 0,
+    "no-multi-spaces": 1,
+    "no-self-compare": 2,
+    "no-sparse-arrays": 2,
+    "no-use-before-define": 2,
+    eqeqeq: 2,
+    "newline-after-var": 2,
+    "no-extra-boolean-cast": 1,
+    "no-extra-parens": 1,
+    "no-empty": 1,
+    "no-unused-vars": 1,
+    "no-dupe-keys": 2,
+    "valid-typeof": 2,
+    "no-unreachable": 2,
+    "key-spacing": [
+      1,
+      {
+        beforeColon: false,
+        afterColon: true
+      }
+    ],
+    "no-redeclare": [
+      2,
+      {
+        // 禁止重复声明变量
+        builtinGlobals: true
+      }
+    ]
   },
   globals: {
     'd': true,
